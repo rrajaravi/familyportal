b694ecd0d1a66326867d3c94291a582d30bfb1cc
Raja Ravi <rajara@cisco.com>
2017-03-09 13:49:32 +0000

    added travis and expense tracker app skeleton

---
+++ expense_tracker/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ExpenseTrackerConfig(AppConfig):
+    name = 'expense_tracker'
---
+++ expense_tracker/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class Test1(TestCase):
+
+   def test_this(self):
+       self.assertEqual(1,1)
